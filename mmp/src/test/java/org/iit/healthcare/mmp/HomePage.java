4391f2dfc6215ca6d7581f69c526094171207705
swati <swati@192.168.1.185>
2022-02-08 01:41:55 -0600

    Pushing the code to the master

---
+++ mmp/src/test/java/org/iit/healthcare/mmp/HomePage.java
@@ -0,0 +1,19 @@
+package org.iit.healthcare.mmp;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+	protected WebDriver driver;
+	public HomePage(WebDriver driver)
+	{
+		this.driver = driver;
+	    if (!driver.getTitle().equals("Home Page of logged in user")) {
+	      throw new IllegalStateException("This is not Home Page of logged in user," +
+	            " current page is: " + driver.getCurrentUrl());
+	    }
+	}
+	public void navigateToAModule(String moduleName)
+	{
+		driver.findElement(By.xpath("//span[normalize-space()='"+moduleName+"']")).click();
+	}
+}
